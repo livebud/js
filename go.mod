c8982656e8943838c66d42b0d06eb410a7fb02e2
Matthew Mueller <mattmuelle@gmail.com>
2023-04-16 13:34:08 -0500

    support concurrent evaluates in both v8 and goja

--- go.mod
+++ go.mod
@@ -5,14 +5,13 @@ go 1.18
 require (
 	github.com/dop251/goja v0.0.0-20220730095050-d11430fb5f72
 	github.com/matryer/is v1.4.0
-	github.com/matthewmueller/text v0.0.0-20210424201111-ec1e4af8dfe8
 	go.kuoruan.net/v8go-polyfills v0.5.1-0.20220727011656-c74c5b408ebd
+	golang.org/x/sync v0.1.0
 	rogchap.com/v8go v0.7.0
 )
 
 require (
 	github.com/dlclark/regexp2 v1.7.0 // indirect
-	github.com/gedex/inflector v0.0.0-20170307190818-16278e9db813 // indirect
 	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- goja/goja.go
+++ goja/goja.go
@@ -3,6 +3,7 @@ package goja
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"github.com/dop251/goja"
 	"github.com/livebud/js"
@@ -11,17 +12,20 @@ import (
 func New(c *js.Console) *VM {
 	vm := goja.New()
 	vm.GlobalObject().Set("console", console(c))
-	return &VM{vm}
+	return &VM{sync.Mutex{}, vm}
 }
 
 type VM struct {
+	mu sync.Mutex
 	vm *goja.Runtime
 }
 
 var _ js.VM = (*VM)(nil)
 
-// Evaluate the expression
+// Evaluate the expression. Evaluate can be called concurrently.
 func (v *VM) Evaluate(ctx context.Context, path, expr string) (string, error) {
+	v.mu.Lock()
+	defer v.mu.Unlock()
 	result, err := v.vm.RunScript(path, expr)
 	if err != nil {
 		return "", fmt.Errorf("goja: error evaluating %s. %w", path, err)
