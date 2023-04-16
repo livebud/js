ef633a94787bb97dfab2238909573a577ded6991
Matt Mueller <mattmuelle@gmail.com>
2023-04-16 13:42:27 -0500

    Support concurrent evaluation in both v8 and goja (#12)
    * support concurrent evaluates in both v8 and goja
    
    * move test into shared compliance test

--- internal/test/compliance.go
+++ internal/test/compliance.go
@@ -6,12 +6,12 @@ import (
 
 	"github.com/livebud/js"
 	"github.com/matryer/is"
+	"golang.org/x/sync/errgroup"
 )
 
 type test struct {
-	Name   string
-	Expect string
-	Func   func(t testing.TB, vm js.VM)
+	Name string
+	Func func(t testing.TB, vm js.VM)
 }
 
 // Compliance tests
@@ -19,6 +19,7 @@ var tests = []*test{
 	newTest("TestEval.js", "10", "2*5"),
 	newTest("TestJSON.js", `{"a":3}`, `JSON.stringify(JSON.parse("{\"a\":3}"))`),
 	newTest("TestDate.js", `2022-08-30T16:12:00.000Z`, `(new Date('2022-08-30T16:12:00.000Z')).toISOString()`),
+	&test{Name: "TestConcurrency", Func: testConcurrency},
 }
 
 // Compliance runs all the tests as subtests
@@ -32,8 +33,7 @@ func Compliance(t *testing.T, vm js.VM) {
 
 func newTest(name, expect, script string) *test {
 	return &test{
-		Name:   name,
-		Expect: expect,
+		Name: name,
 		Func: func(t testing.TB, vm js.VM) {
 			t.Helper()
 			is := is.New(t)
@@ -46,4 +46,26 @@ func newTest(name, expect, script string) *test {
 			is.Equal(result, expect)
 		},
 	}
+}
+
+func testConcurrency(t testing.TB, vm js.VM) {
+	is := is.New(t)
+	ctx := context.Background()
+	value, err := vm.Evaluate(ctx, "math.js", `const multiply = (a, b) => a * b`)
+	is.NoErr(err)
+	is.Equal("undefined", value)
+	eg := new(errgroup.Group)
+	for i := 0; i < 100; i++ {
+		eg.Go(func() error {
+			value, err := vm.Evaluate(ctx, "run.js", "multiply(3, 2)")
+			if err != nil {
+				return err
+			}
+			if value != "6" {
+				return err
+			}
+			return nil
+		})
+	}
+	is.NoErr(eg.Wait())
 }
